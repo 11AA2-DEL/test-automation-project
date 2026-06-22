15c3f598e29266021bef12e6bec2dacce3b55756
测试111zx <1445563962@qq.com>
2026-06-22 23:48:45 +0800

    User接口、异常场景、YAML数据补充

--- test_cases/test_posts.py
+++ test_cases/test_posts.py
@@ -8,12 +8,48 @@ class TestPosts:
         resp = api.get("/posts/1")
         assert resp.status_code == 200
         assert resp.json()["id"] == 1
+        assert "title" in resp.json()
+
 
     def test_create(self,api):
         body = {"title":"测试","body":"内容","userid": 1}
         resp = api.post("/posts",body)
         assert resp.status_code == 201
 
+#这条用例是做异常场景覆盖，保证程序对非法 / 不存在 ID 有正确处理逻辑，不会报错崩溃。
     def test_not_found(self, api):
             resp = api.get("/posts/99999")
             assert resp.status_code == 404
+
+    def test_get_by_user(self,api):
+        resp = api.get("/posts?userId = 1")
+        assert resp.status_code ==200
+        for item in resp.json():
+            assert item["userId"] == 1
+
+    def test_get_user(self,api):
+        resp = api.get("/users")
+        assert resp.status_code == 200
+        data = resp.json()
+        assert isinstance(data,list)
+        assert len(data) >0
+        first_user = data [0]
+        assert "name" in first_user
+class TestComments:
+    def test_get_all(self,api):
+        resp = api.get("/comments")
+        assert resp.status_code ==200
+        assert len(resp.json()) > 0
+    def test_get_comments(self,api):
+        resp = api.get("/comments?postId=1")
+        assert resp.status_code ==200
+        for ietm in resp.json():
+            assert ietm["postId"] == 1
+
+    def test_conmment_has_email(self,api):
+        resp = api.get("/comments/1")
+        assert resp.status_code ==200
+        data = resp.json()
+        assert "email" in data
+        assert "@" in data["email"]
+
--- utils/logger.py
+++ utils/logger.py
@@ -1,16 +1,18 @@
+#
 import logging
 import os
 from config import LOG_LEVEL, LOG_DIR
 os.makedirs(LOG_DIR, exist_ok=True)
-def get_logger(name):
+def get_logger(name): #创建一个日志器
     logger = logging.getLogger(name)
     logger.setLevel(LOG_LEVEL)
+    #配置日志格式：时间、模块名、级别、消息
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch = logging.StreamHandler()
+    ch = logging.StreamHandler() #输出到终端
     ch.setLevel(LOG_LEVEL)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-    fh =  logging.FileHandler(os.path.join(LOG_DIR, "test.log"),encoding = "utf-8")
+    fh =  logging.FileHandler(os.path.join(LOG_DIR, "test.log"),encoding = "utf-8")  #输出到文件
     fh.setLevel(LOG_LEVEL)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
